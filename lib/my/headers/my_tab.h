9b95c137f253f7e6db0e7ec764a39b24ace8d336
Roméo Poirier <romeo.poirier@epitech.eu>
2025-04-28 09:03:19 +0200

    new (my_tab lib) add str_tabnullify func, remove all values from a char ** and choose wether you free them or not

--- lib/my/headers/my_tab.h
+++ lib/my/headers/my_tab.h
@@ -33,4 +33,14 @@ int str_puttab(char **tab, char *separator);
 //on success: pointer to str list
 //on error: NULL
 char **str_extendtab(char **tab, int nmemb);
+
+//Nullifies all values inside a list of string
+//@param tab list of string to nullify
+//@param free_them set to 1 to free the values, 0 to just set them to NULL
+//@returns 0
+//OR -1 if tab is NULL
+//@note This function doesn't free the tab, refer to free_strtab for that.
+//Be sure to keep in mind the length of your tab as str_tablen will return 0
+//if called on a nullified tab
+int str_tabnullify(char **tab, int free_them);
 #endif /* MY_TAB_H */
